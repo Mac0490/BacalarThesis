eff4f2498370139e648a43cd29198887741c39d1
Mac0490 <72305496+Mac0490@users.noreply.github.com>
2021-04-27 17:06:48 +0200

    final version

---
+++ 2021_MAC0490_BP/app6/app/app/src/main/java/com/example/app/Screenshot.java
@@ -0,0 +1,52 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.PixelCopy;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.sceneform.ArSceneView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Screenshot {
+
+    String generateFileName() {
+        String date = new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + date + "_screenshot.jpg";
+    }
+
+    public void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+        File outFile = new File(filename);
+        if (!outFile.getParentFile().exists()) {
+            outFile.getParentFile().mkdirs();
+        }
+
+            try (FileOutputStream outputStream = new FileOutputStream(filename);
+                 ByteArrayOutputStream outputData = new ByteArrayOutputStream()) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputData);
+                outputData.writeTo(outputStream);
+                outputStream.close();
+                outputStream.flush();
+
+            } catch (IOException ex) {
+                throw new IOException("Failed to save bitmap to disk - ERROR " + ex, ex);
+            }
+
+
+
+    }
+
+}
